40e6abe50526794a77109d3061790cf9673b575b
Cda <murillo003@hotmail.com>
2026-08-14 10:32:10 -0300

    Novo favicon customizado e nova foto de perfil

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4036988526" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2840753321" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
